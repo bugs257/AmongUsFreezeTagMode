8594f54819759fab0b6f3c8eb2afea3ca0bcad77
bugs257 <bugs257257@gmail.com>
2026-02-27 22:28:49 -0600

    Implement win condition methods for Crewmates and Hunter

--- Systems/WinConditionSystem.cs
+++ Systems/WinConditionSystem.cs
@@ -1,1 +1,17 @@
+public static class WinConditionSystem
+{
+    public static void CrewmateWin()
+    {
+        EndGame(GameOverReason.CrewmatesByTask);
+    }
 
+    public static void HunterWin()
+    {
+        EndGame(GameOverReason.ImpostorByKill);
+    }
+
+    private static void EndGame(GameOverReason reason)
+    {
+        AmongUsClient.Instance.RpcEndGame(reason, false);
+    }
+}
