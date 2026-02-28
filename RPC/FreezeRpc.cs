2b592a032e816a9fdb1daea4d3dea44afd724198
bugs257 <bugs257257@gmail.com>
2026-02-27 22:22:02 -0600

    Add FreezeRpc class with freeze/unfreeze methods

--- RPC/FreezeRpc.cs
+++ RPC/FreezeRpc.cs
@@ -1,1 +1,28 @@
+public static class FreezeRpc
+{
+    public static void SendFreeze(byte playerId)
+    {
+        MessageWriter writer =
+            AmongUsClient.Instance.StartRpcImmediately(
+                PlayerControl.LocalPlayer.NetId,
+                70,
+                SendOption.Reliable,
+                -1);
 
+        writer.Write(playerId);
+        AmongUsClient.Instance.FinishRpcImmediately(writer);
+    }
+
+    public static void SendUnfreeze(byte playerId)
+    {
+        MessageWriter writer =
+            AmongUsClient.Instance.StartRpcImmediately(
+                PlayerControl.LocalPlayer.NetId,
+                71,
+                SendOption.Reliable,
+                -1);
+
+        writer.Write(playerId);
+        AmongUsClient.Instance.FinishRpcImmediately(writer);
+    }
+}
