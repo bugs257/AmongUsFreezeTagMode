18703f99b167fd4ced19bc112c2afdd0d792c4c7
bugs257 <bugs257257@gmail.com>
2026-02-27 22:30:01 -0600

    Add patch to reduce task time for non-hunters

--- Patches/TaskCompletePatch.cs
+++ Patches/TaskCompletePatch.cs
@@ -1,1 +1,11 @@
+[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CompleteTask))]
+class TaskCompletePatch
+{
+    static void Postfix(PlayerControl __instance, uint taskId)
+    {
+        if (!AmongUsClient.Instance.AmHost) return;
+        if (__instance == FreezeTagGameManager.Hunter) return;
 
+        RoundTimerSystem.ReduceTimeFromTask();
+    }
+}
