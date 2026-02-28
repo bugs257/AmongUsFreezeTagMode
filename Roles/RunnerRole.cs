4743b2b4d7bc317ec585692a8b3b64f8fe11b49c
bugs257 <bugs257257@gmail.com>
2026-02-27 22:23:02 -0600

    Add RunnerRole class extending CrewmateRole

--- Roles/RunnerRole.cs
+++ Roles/RunnerRole.cs
@@ -1,1 +1,7 @@
+public sealed class RunnerRole : CrewmateRole
+{
+    public RunnerRole(IntPtr ptr) : base(ptr) { }
 
+    public override string Name => "Runner";
+    public override Color RoleColor => new(0f, 0.6f, 1f);
+}
