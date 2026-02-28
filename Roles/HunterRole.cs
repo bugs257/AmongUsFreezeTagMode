02e3692980420e24ff4d515052ad514f475deb7d
bugs257 <bugs257257@gmail.com>
2026-02-27 22:22:36 -0600

    Add HunterRole class extending ImpostorRole

--- Roles/HunterRole.cs
+++ Roles/HunterRole.cs
@@ -1,1 +1,7 @@
+public sealed class HunterRole : ImpostorRole
+{
+    public HunterRole(IntPtr ptr) : base(ptr) { }
 
+    public override string Name => "Hunter";
+    public override Color RoleColor => new(1f, 0f, 0f);
+}
