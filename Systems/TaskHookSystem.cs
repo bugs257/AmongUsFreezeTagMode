3c116650eb14b4ecbbcf754db0678e15fb255348
bugs257 <bugs257257@gmail.com>
2026-03-04 18:54:40 -0600

    Create TaskHookSystem.cs

---
+++ Systems/TaskHookSystem.cs
@@ -0,0 +1,12 @@
+namespace AmongUsFreezeTagMode.Systems
+{
+    public static class TaskHookSystem
+    {
+        public static float TimeReductionPerTask = 7f;
+
+        public static void EnableTaskHooks()
+        {
+            HarmonyPatches.ApplyPatches();
+        }
+    }
+}
