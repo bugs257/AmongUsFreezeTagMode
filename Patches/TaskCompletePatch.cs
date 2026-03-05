570af1daa7f6539ea0d53f8221e96b7ebf5f0934
bugs257 <bugs257257@gmail.com>
2026-03-04 18:56:40 -0600

    Update TaskCompletePatch.cs

--- Patches/TaskCompletePatch.cs
+++ Patches/TaskCompletePatch.cs
@@ -1,11 +1,18 @@
-[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CompleteTask))]
-class TaskCompletePatch
-{
-    static void Postfix(PlayerControl __instance, uint taskId)
-    {
-        if (!AmongUsClient.Instance.AmHost) return;
-        if (__instance == FreezeTagGameManager.Hunter) return;
+using HarmonyLib;
 
-        RoundTimerSystem.ReduceTimeFromTask();
+namespace AmongUsFreezeTagMode.Patches
+{
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CompleteTask))]
+    public static class TaskCompletePatch
+    {
+        static void Postfix(PlayerControl __instance, uint taskId)
+        {
+            if (!AmongUsClient.Instance.AmHost) return;
+            if (__instance == FreezeTagGameManager.Hunter) return;
+
+            Systems.RoundTimerSystem.ReduceTimeFromTask(
+                Systems.TaskHookSystem.TimeReductionPerTask
+            );
+        }
     }
 }
